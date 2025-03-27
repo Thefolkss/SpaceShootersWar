2af3639bf8503292f44ae52d560f48e19d7acf7b
Thefolkss <106935338+Thefolkss@users.noreply.github.com>
2025-03-27 14:02:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode(size=(1024, 768))
+print('Setup Start')
+
+print('Loop Start')
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+         pygame.quit()  # Close Window
+         quit()  # End pygame
